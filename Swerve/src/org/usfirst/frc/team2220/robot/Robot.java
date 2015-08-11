50bf2a6081fcc93b3c6cd5dd237e3d6b7e773c80
potatoe420 <alder314@gmail.com>
2015-08-10 22:26:51 -0500

    Added PIDController object

--- Swerve/src/org/usfirst/frc/team2220/robot/Robot.java
+++ Swerve/src/org/usfirst/frc/team2220/robot/Robot.java
@@ -11,36 +11,85 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Robot extends SampleRobot{
 	
     Joystick stick;
-    SModule frontRight;
-    AnalogInput potatoe;
+    SModule backleft;
+    AnalogInput rot_bl;
+    CANTalon motor_bl;
     SmartDashboard board;
-    double analogVal;
+    
+    double rot_blVal;
     
     public Robot() {
-        stick = new Joystick(0);
-        potatoe = new AnalogInput(0);
+        //3395
+        motor_bl = new CANTalon(8);
+        rot_bl = new AnalogInput(0);
+        backleft = new SModule(motor_bl, rot_bl);
         board = new SmartDashboard();
+        stick = new Joystick(0);
     } 
 
     /**
-     * Nothing rn
+     * Nothing rn void 
      */
     public void autonomous() {
-        
+    	
     }
 
     /**
-     * Runs the motors with cool steering
+     * Start swerve tesgin
      */
     public void operatorControl() {
-        //myRobot.setSafetyEnabled(true);
         while (isOperatorControl() && isEnabled()) {
-            //myRobot.arcadeDrive(stick); // drive with arcade style (use right stick)
-        	analogVal = potatoe.getAverageVoltage();
-        	board.putNumber("AnalogVal", analogVal);
-            Timer.delay(0.005);		// wait for a motor update time
-        }
+    		/*
+        	if(stick.getRawAxis(2) < -0.5)
+        		backleft.setRot(0);
+        	else
+        		backleft.setRot(3.0);
+        		*/
+        	backleft.setRot(stick.getRawAxis(5) * 3);
+
+        	board.putNumber("Err", backleft.getErr());
+    		board.putNumber("AnalogValue", rot_bl.getAverageValue());
+    		board.putNumber("MotorVal", motor_bl.get());
+    		Timer.delay(0.005);	
+    	}
     }
+    /**
+     * Runs the motors with cool steering
+     */
+    /*
+    public void testEncoders() {
+        //myRobot.setSafetyEnabled(true);
+        //while (isOperatorControl() && isEnabled()) {
+            //myRobot.arcadeDrive(stick); // drive with arcade style (use right stick)
+        	
+        	//getValue = 0 to ~39000
+        	//getAccumulatedValue = continually adds the getValue
+        	//getVoltage = 0 to 5 volts
+        	//getAccumulatorCount = how many times the accumulator has counted
+        	//pidGet = with no pid control, just the voltage
+        	//getAverageValue = sample sizes the getValue
+        	board.putNumber("AnalogVoltage", potatoe.getAverageVoltage());
+        	board.putNumber("AnalogValue", potatoe.getAverageValue());
+        	
+        	digVal = digEncoder.get();
+        	board.putNumber("DigVal", digVal);
+        	
+        	gyroRate = dubBoard.getRate();
+        	board.putNumber("Gyro Rate ", gyroRate);
+        	
+        	gyroAngle = dubBoard.getAngle();
+        	board.putNumber("Gyro Angle ", gyroAngle);
+        	/*
+        	if(analogVal > 2.0 && analogVal < 3.0)
+        		motorRun.set(0.3);
+        	else
+        		motorRun.set(0);
+        	///
+        	
+            Timer.delay(0.005);		// wait for a motor update time
+        //}
+    }
+     */
 
     /**
      * Runs during test mode
--- Swerve/src/org/usfirst/frc/team2220/robot/SModule.java
+++ Swerve/src/org/usfirst/frc/team2220/robot/SModule.java
@@ -1,17 +1,19 @@
 package org.usfirst.frc.team2220.robot;
 
 import edu.wpi.first.wpilibj.AnalogInput;
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.PIDController;
+import edu.wpi.first.wpilibj.TalonSRX;
 
 public class SModule {
 	
-	Talon speedMotor;
+	CANTalon speedMotor;
 	Encoder speedEnc;
 	PIDController speed;
 	
-	Talon rotMotor;
+	CANTalon rotMotor;
 	AnalogInput rotEnc; //Encoder rotEnc;
 	PIDController rotation;
 	
@@ -22,10 +24,10 @@ public class SModule {
 	 * @param sTalon Talon for speed control
 	 * @param sEnc Encoder for speed control
 	 * @param rTalon Talon for rotational control
-	 * @param rEnc Encoder for rotational controll
+	 * @param rEnc Encoder for rotational control
 	 * 
 	 */
-	public SModule(Talon sTalon, Encoder sEnc, Talon rTalon, AnalogInput rEnc)
+	public SModule(CANTalon sTalon, Encoder sEnc, CANTalon rTalon, AnalogInput rEnc)
 	{
 		speedEnc = sEnc;
 		speedMotor = sTalon;
@@ -38,13 +40,33 @@ public class SModule {
 	}
 	
 	/**
+	 * Constructor for testing only rotation
+	 * @param rTalon Talon for rotational control
+	 * @param rEnc Encoder for rotational control
+	 */
+	public SModule(CANTalon rTalon, AnalogInput rEnc)
+	{
+		rotMotor = rTalon;
+		rotEnc = rEnc;
+		rotation = new PIDController(0.05, 0.001, 0, rotEnc, rotMotor);
+		rotation.enable();
+		rotation.setOutputRange(-0.2, 0.2);
+		rotation.setAbsoluteTolerance(0.01);
+	}
+	
+	/**
 	 * 0 is straight forward, 360 is also straight forward
 	 * @param degrees set the degrees relative to 0
 	 */
 	public void setRot(double degrees)
 	{
-		degrees = degrees * degrees / degrees; //math
+		degrees = degrees; //math
 		rotation.setSetpoint(degrees);
+	}
+	
+	public double getErr()
+	{
+		return rotation.getError();
 	}
 	
 	/**
